0787e9d2ef818eb76efeb9ae47c11a63882935e3
Afonso R. Costa Jr <arabelo@gmail.com>
2019-02-05 17:44:07 -0400

    Put a vertical plane on the scene

--- app/src/main/java/com/example/com/arvideoplayer/Main.java
+++ app/src/main/java/com/example/com/arvideoplayer/Main.java
@@ -1,7 +1,13 @@
 package com.example.com.arvideoplayer;
 
+import android.opengl.GLES30;
+
 import com.samsungxr.SXRContext;
 import com.samsungxr.SXRMain;
+import com.samsungxr.SXRMaterial;
+import com.samsungxr.SXRMesh;
+import com.samsungxr.SXRNode;
+import com.samsungxr.SXRRenderData;
 import com.samsungxr.mixedreality.IMixedReality;
 import com.samsungxr.mixedreality.IMixedRealityEvents;
 import com.samsungxr.mixedreality.IPlaneEvents;
@@ -13,12 +19,14 @@ import com.samsungxr.utility.Log;
 public class Main extends SXRMain {
 
     private final String TAG = Main.class.getSimpleName();
-    private SXRMixedReality mMixedReality;
+    private SXRContext mContext;
 
     @Override
     public void onInit(SXRContext sxrContext) {
 
-        mMixedReality = new SXRMixedReality(sxrContext.getMainScene());
+        mContext = sxrContext;
+
+        SXRMixedReality mMixedReality = new SXRMixedReality(sxrContext.getMainScene());
         mMixedReality.getEventReceiver().addListener(planeEventsListener);
         mMixedReality.getEventReceiver().addListener(mixedRealityEventsListener);
         mMixedReality.resume();
@@ -33,6 +41,9 @@ public class Main extends SXRMain {
         @Override
         public void onPlaneDetected(SXRPlane plane) {
             Log.d(TAG, "on plane detected");
+            SXRNode planeNode = createPlaneNode();
+            planeNode.attachComponent(plane);
+            mContext.getMainScene().addNode(planeNode);
         }
 
         @Override
@@ -47,7 +58,10 @@ public class Main extends SXRMain {
 
         @Override
         public void onPlaneGeometryChange(SXRPlane plane) {
+            SXRMesh mesh = new SXRMesh(getSXRContext());
+            mesh.setVertices(plane.get3dPolygonAsArray());
 
+            plane.getOwnerObject().getRenderData().setMesh(mesh);
         }
     };
 
@@ -67,4 +81,22 @@ public class Main extends SXRMain {
 
         }
     };
+
+    private SXRNode createPlaneNode() {
+        SXRMaterial mat = new SXRMaterial(mContext, SXRMaterial.SXRShaderType.Phong.ID);
+        mat.setDiffuseColor(1, 1, 1, 0.5f);
+
+        SXRRenderData renderData = new SXRRenderData(mContext);
+        renderData.disableLight();
+        renderData.setAlphaBlend(true);
+        renderData.setRenderingOrder(SXRRenderData.SXRRenderingOrder.TRANSPARENT);
+        renderData.setDrawMode(GLES30.GL_TRIANGLE_FAN);
+        renderData.setMaterial(mat);
+
+        SXRNode plane = new SXRNode(mContext);
+        plane.attachComponent(renderData);
+        plane.setName("Plane");
+
+        return plane;
+    }
 }
